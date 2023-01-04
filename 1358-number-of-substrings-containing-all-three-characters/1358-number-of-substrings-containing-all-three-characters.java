4825bac93b7e43d1613139fa0c2ad71b564db9fe
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2023-01-04 19:01:55 +0530

    Time: 21 ms (53.42%), Space: 42.4 MB (82.14%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,19 +1,17 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         long n=s.length();int j=0; long count=0;
-        HashMap<Character,Integer>hs=new HashMap<>();
+        int countA,countB,countC;countA=countB=countC=0;
         for(int i=0;i<n;i++){
-            while(!hs.containsKey(s.charAt(i))&&hs.size()==2){
-                if(hs.get(s.charAt(j))==1)
-                    hs.remove(s.charAt(j));
-                else
-                    hs.put(s.charAt(j),hs.get(s.charAt(j))-1);
+            while((s.charAt(i)=='a'&&countB>=1&&countC>=1)||(s.charAt(i)=='b'&&countA>=1&&countC>=1)||(s.charAt(i)=='c'&&countA>=1&&countB>=1)){
+                if(s.charAt(j)=='a')countA--;
+                else if(s.charAt(j)=='b')countB--;
+                else countC--;
                 j++;
             }
-            if(hs.containsKey(s.charAt(i)))
-                hs.put(s.charAt(i),hs.get(s.charAt(i))+1);
-            else
-                hs.put(s.charAt(i),1);
+            if(s.charAt(i)=='a')countA++;
+            else if(s.charAt(i)=='b')countB++;
+            else countC++;
             count+=i-j+1;
         }
         return (int)(n*(n+1)/2-count);
