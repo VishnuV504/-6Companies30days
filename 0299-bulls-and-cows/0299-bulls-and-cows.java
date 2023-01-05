80185441725b828fdceb39f79f4262b99764f33d
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2023-01-05 11:22:41 +0530

    Time: 12 ms (35.18%), Space: 42.9 MB (40.61%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -1,26 +1,21 @@
 class Solution {
     public String getHint(String secret, String guess) {
-        int count=0; int n=secret.length();HashMap<Character,Integer>hs=new HashMap<>();
+        int count=0; int n=secret.length();int freq[]=new int[10];
         int i=0;String ans="";
         while(i<n){
             if(secret.charAt(i)==guess.charAt(i))
                 count++;
-            else{
-                if(hs.containsKey(guess.charAt(i)))
-                    hs.put(guess.charAt(i),hs.get(guess.charAt(i))+1);
-                else
-                    hs.put(guess.charAt(i),1);
-            }
+            else
+                freq[guess.charAt(i)-48]++;
             i++;
         }
         i=0;ans+=count;
         ans+="A"; count=0;
         while(i<n){
             if(secret.charAt(i)!=guess.charAt(i)){
-                if(hs.containsKey(secret.charAt(i))){
+                if(freq[secret.charAt(i)-48]>0){
                     count++;
-                    hs.put(secret.charAt(i),hs.get(secret.charAt(i))-1);
-                    if(hs.get(secret.charAt(i))==0)hs.remove(secret.charAt(i));
+                    freq[secret.charAt(i)-48]--;
                 }
             }
             i++;
