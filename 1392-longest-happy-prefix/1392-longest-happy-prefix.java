3747d6ce195e3dfac36a8cfb382521acf5d03f1f
VishnuV504 <97045581+VishnuV504@users.noreply.github.com>
2023-01-06 11:18:39 +0530

    Time: 26 ms (48.58%), Space: 54.5 MB (24.11%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int n=s.length(); int i=1; int j=0;
+        int pre[]=new int[n];int max=0;
+        while(i<n){
+            if(s.charAt(i)!=s.charAt(j)){
+                i++;
+            }
+            else{
+                while(i<n&&s.charAt(i)==s.charAt(j)){
+                    pre[i]=j+1;
+                    i++;
+                    j++;
+                }
+                while(i<n&&s.charAt(i)!=s.charAt(j)&&j!=0){
+                    j=pre[j-1];
+                }
+            }
+        }
+        return s.substring(0,pre[n-1]);
+    }
+}
